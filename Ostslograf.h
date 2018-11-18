9563a0f251429c69012eea1a79ef8bee1f1f2ff1
Oleh Zherebukh <info@developerS2.com>
2018-11-18 11:40:49 +0200

    Declaration of class Ostslograf which contains 7 fields (3 public, 2 protected, 2 private) and 5 methods

---
+++ Ostslograf.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "pch.h"
+#include <iostream>
+#include <math.h>
+#include <string>
+using namespace std;
+
+
+
+class Ostslograf {
+private:
+	double frequency;
+	double memory;
+	string brand;
+public:
+	Ostslograf();
+	Ostslograf(double f, double m, string b, int pod, string name, double f1, double f2);
+	~Ostslograf();
+	int podilka;
+	string name;
+	double getfrequency();
+	double getmemory();
+	string getbrand();
+	double getfrequency1();
+	double getfrequency2();
+	
+protected:
+	double frequency1;
+	double frequency2;
+};
